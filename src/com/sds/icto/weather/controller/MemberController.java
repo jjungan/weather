d96cc42679caf1f3bb66b4e6557b8e5dc3b0aafb
student <student@M140417>
2015-05-26 15:05:47 +0900

    jung

--- src/com/sds/icto/weather/controller/MemberController.java
+++ src/com/sds/icto/weather/controller/MemberController.java
@@ -1,5 +1,8 @@
 package com.sds.icto.weather.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sds.icto.weather.domain.MemberVo;
 import com.sds.icto.weather.service.MemberService;
@@ -52,6 +56,21 @@ public class MemberController {
 		session.removeAttribute("authMember");
 		session.invalidate();
 		return "redirect:/index";
+	}
+	
+	@RequestMapping(value="/checkemail", method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> checkEmail(MemberVo vo){
+		Map<String, Object> map = new HashMap<String, Object>();
+		MemberVo m = memberService.selectMemberByEmail(vo);
+		if(m == null){ //중복되는 이메일 없음
+			map.put("flag", true);
+			map.put("msg", "사용가능한 이메일입니다.");
+		}else{ //중복되는 이메일있음
+			map.put("flag", false);
+			map.put("msg", "중복되는 이메일이 있습니다.");
+		}
+		return map;
 	}
 	
 	
